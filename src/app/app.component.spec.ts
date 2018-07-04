1bf15868390284a3366bd35d09513109778838b3
Maier, Martin <martin.maier@saxsys.de>
2018-07-04 16:19:50 +0200

    * nested contacts to submodule

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AppComponent} from './app.component';
+import {ContactsModule} from './contacts/contacts.module';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -8,6 +9,7 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ContactsModule],
       declarations: [AppComponent]
     })
       .compileComponents();
