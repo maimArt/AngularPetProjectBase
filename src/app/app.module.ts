c95a951398dd017cab834ca1d3bb3622a98e288d
Maier, Martin <martin.maier@saxsys.de>
2018-07-04 16:19:50 +0200

    * nested contacts to submodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {ContactCreatorComponent} from './contacts/contact-creator/contact-creator.component';
-import {ContactListComponent} from './contacts/contact-list/contact-list.component';
-import {ContactService} from './contacts/services/contact.service';
+import {ContactsModule} from './contacts/contacts.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    ContactCreatorComponent,
-    ContactListComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule, ContactsModule
   ],
-  providers: [ContactService],
-  bootstrap: [ContactCreatorComponent, ContactListComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
